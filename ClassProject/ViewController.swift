942d06621a21bd133dc8293d05ff0a2310f7eb09
Shashank Ginjpalli <shashank.ginjpalli@gmail.com>
2019-10-30 17:01:19 -0700

    Initial Commit

---
+++ ClassProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClassProject
+//
+//  Created by Shashank Ginjpalli on 10/30/19.
+//  Copyright © 2019 Shashank Ginjpalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
