74cd97dedc11faab5cfb59213b677d948984b1f4
Shashank Ginjpalli <shashank.ginjpalli@gmail.com>
2019-11-15 00:33:57 -0700

    webapi call for search

--- ClassProject/AddView.swift
+++ ClassProject/AddView.swift
@@ -37,5 +37,36 @@ class AddView: UIViewController, UITableViewDataSource, UITableViewDelegate {
         return cell
     }
 
+    @IBAction func getData(_ sender: Any) {
+        let movieName = searchField.text;
+        
+        let urlAsString = "https://api.themoviedb.org/3/search/movie?api_key=bf2c357d63cf76d216fb334f6083eeae&language=en-US&query="+movieName!+"&page=1&include_adult=false"
+        
+        
+      let url = URL(string: urlAsString)!
+      let urlSession = URLSession.shared
+      
+      
+      let jsonQuery = urlSession.dataTask(with: url, completionHandler: { data, response, error -> Void in
+        if (error != nil) {
+            print(error!.localizedDescription)
+        }
+        var err: NSError?
 
+
+        var jsonResult = (try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)) as! NSDictionary
+        if (err != nil) {
+            print("JSON Error \(err!.localizedDescription)")
+        }
+
+        print(jsonResult)
+
+        })
+        
+        jsonQuery.resume()
+        
+    print("Pressed")
+        
+    }
+    
 }
---
+++ ClassProject/Model/searchResults.swift
@@ -0,0 +1,9 @@
+//
+//  searchResults.swift
+//  ClassProject
+//
+//  Created by Shashank Ginjpalli on 11/15/19.
+//  Copyright © 2019 Shashank Ginjpalli. All rights reserved.
+//
+
+import Foundation
